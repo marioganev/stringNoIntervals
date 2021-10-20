c08280df9b168ca2c03c0b2e5257216243e21b92
User 14 <User 14@PC15>
2021-10-20 14:41:41 +0300

    Add project files.

---
+++ stringNoIntervals/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace stringNoIntervals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine(str = str.Replace(" ", String.Empty));
+        }
+    }
+}
